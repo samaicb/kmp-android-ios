86445f577db977ca034c3304fb1e5b0056adcc04
samaicb <166864166+samaicb@users.noreply.github.com>
2024-04-15 12:51:50 -0700

    kmp-Android-ios

---
+++ App.kt
@@ -0,0 +1,169 @@
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.animation.animateContentSize
+import androidx.compose.material.Surface
+import androidx.compose.material.Text as Text1
+
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        Conversation(SampleData.conversationSample)
+
+    }
+
+}
+data class Message(val author: String, val body: String)
+@Composable
+fun MessageCard(msg: Message){
+    // Add padding around our message
+    Row (modifier = Modifier.padding(all = 8.dp)){
+        /*Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = null,
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.primary, CircleShape)
+        )*/
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text1(
+                text = msg.author,
+                color = MaterialTheme.colors.secondary,
+                style = MaterialTheme.typography.subtitle1
+            )
+            // Add a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text1(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body1
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Lexi",
+            "Test...Test...Test..."
+        ),
+        Message(
+            "Lexi",
+            """List of Android versions:
+            |Android KitKat (API 19)
+            |Android Lollipop (API 21)
+            |Android Marshmallow (API 23)
+            |Android Nougat (API 24)
+            |Android Oreo (API 26)
+            |Android Pie (API 28)
+            |Android 10 (API 29)
+            |Android 11 (API 30)
+            |Android 12 (API 31)""".trim()
+        ),
+        Message(
+            "Lexi",
+            """I think Kotlin is my favorite programming language.
+            |It's so much fun!""".trim()
+        ),
+        Message(
+            "Lexi",
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            "Lexi",
+            """Hey, take a look at Jetpack Compose, it's great!
+            |It's the Android's modern toolkit for building native UI.
+            |It simplifies and accelerates UI development on Android.
+            |Less code, powerful tools, and intuitive Kotlin APIs :)""".trim()
+        ),
+        Message(
+            "Lexi",
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            "Lexi",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            "Lexi",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            "Lexi",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            "Lexi",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            "Lexi",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "Lexi",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "Lexi",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
+
+}
